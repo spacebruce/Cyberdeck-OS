7f8ddd280bef13ced5a898bf8a532f90b0e3eade
spacebruce <noel.abercrombie@hotmail.com>
2022-06-20 15:56:16 +0100

    Add test program

---
+++ Firmware/Cyberdeck-Test/src/Firmware/App/AppHeader.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "..\Gui\Icon.hpp"
+
+namespace Firmware
+{
+    struct Appheader
+    {
+    //Aesthetic
+        char Name[16];
+        GUI::Icon Icon;
+    //OS
+        void* EntryPoint;
+    }
+}
