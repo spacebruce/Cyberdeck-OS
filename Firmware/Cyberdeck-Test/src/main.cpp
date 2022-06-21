97465f092b60d56d62c52d4b72b61d790eaf1ed5
spacebruce <noel.abercrombie@hotmail.com>
2022-06-21 01:11:35 +0100

    Multithreading test

--- Firmware/Cyberdeck-Test/src/main.cpp
+++ Firmware/Cyberdeck-Test/src/main.cpp
@@ -1,11 +1,30 @@
 #include <Arduino.h>
 
+#include "Software\Test1\Test.hpp"
+#include "Software\Test2\Test.hpp"
+
 void setup()
 {
+    Serial.begin(115200);
+    Serial.println("Startup");
+
+    xTaskCreate(TestApp1::Run,
+        "TestApp1",
+        1024,
+        nullptr,
+        1,
+        nullptr
+    );
+    xTaskCreate(TestApp2::Run,
+        "TestApp1",
+        1024,
+        nullptr,
+        1,
+        nullptr
+    );
 
 }
 
 void loop()
 {
-    
 }
